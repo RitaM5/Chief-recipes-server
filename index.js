55191f77028f07dbaac50c78e80c4bfcaf381c64
RitaM5 <sporshia108514orva@gmail.com>
2023-05-02 13:40:23 +0600

    serviser site added

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const recipes = "./data/recipe.json"
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Server is running...");
+});
+app.get('/recipe', (req, res) => {
+    res.send(recipes);
+});
+/*
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+});
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+}); */
+app.listen(port, () => {
+    console.log(`recipe api running on port: ${port} `);
+});
