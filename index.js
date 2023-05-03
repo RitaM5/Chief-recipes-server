8410b499d406eb865ac1bccfeba1c8e5c9ef50df
RitaM5 <sporshia108514orva@gmail.com>
2023-05-03 12:05:51 +0600

    cooking method added in json data

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const port = process.env.PORT || 5000;
 const recipes = require("./data/recipe.json");
 const cors = require('cors');
 
-app.use(cors());
-
+//app.use(cors());
+const corsConfig = {
+    origin: '*',
+    Credential: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig));
 app.get('/', (req, res) => {
     res.send("Server is running...");
 });
 app.get('/recipe', (req, res) => {
     res.send(recipes);
 });
-app.get('/chef/:id', (req, res) => {
+app.get('/recipe/:id', (req, res) => {
     const id = req.params.id;
-    const recipeDetails = recipes.find(r => r.id === id);
+    console.log(id);
+    const recipeDetails = recipes?.find(r => r.id == id);
     res.send(recipeDetails)
 });
 /*
