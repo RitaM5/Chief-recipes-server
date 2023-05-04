94566dd76ef91887b492937bdeab7af3e0df6366
RitaM5 <sporshia108514orva@gmail.com>
2023-05-04 22:49:39 +0600

    code cleaned

--- index.js
+++ index.js
@@ -23,26 +23,6 @@ app.get('/recipe/:id', (req, res) => {
     const recipeDetails = recipes?.find(r => r.id == id);
     res.send(recipeDetails)
 });
-/*
-https://chef-recipe-server-rita5cmt1b108514-gmailcom.vercel.app/recipe
-app.get('/news', (req, res) => {
-    res.send(news);
-});
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews)
-});
-app.get('/categories/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (id === 0) {
-        res.send(news);
-    }
-    else {
-        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
-        res.send(categoryNews)
-    }
-}); */
 app.listen(port, () => {
     console.log(`recipe api running on port: ${port} `);
 });
